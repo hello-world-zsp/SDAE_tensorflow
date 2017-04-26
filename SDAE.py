52128b969c5eb716bd22ac044e764d7eff2e0d67
hello-world-zsp <zhangsppp@gmail.com>
2017-04-26 18:09:17 +0800

    修改输出特征图部分
    使用各层w作为特征图。。。大小都是28*28
    - w1
    - w1*w2
    - w1*w2*w3
    - ...

--- SDAE.py
+++ SDAE.py
@@ -3,6 +3,21 @@ import tensorflow as tf
 from utils import *
 from DAE import *
 
+class SummaryHandle():
+    def __init__(self):
+        self.summ_enc_w = []
+        self.summ_enc_b = []
+        self.summ_dec_w = []
+        self.summ_dec_b = []
+        self.summ_loss = []
+
+    def add_summ(self,e_w,e_b,d_w,d_b):
+        self.summ_enc_w.append(e_w)
+        self.summ_enc_b.append(e_b)
+        self.summ_dec_w.append(d_w)
+        self.summ_dec_b.append(d_b)
+
+
 class SDAE(object):
     def __init__(self, sess, input_size, noise=0, n_nodes=(180, 42, 10), learning_rate=1,
                  n_epochs=100,is_training = True, input_dim = 1,
@@ -37,17 +52,21 @@ class SDAE(object):
     def build(self,is_training=True):
         self.hidden_layers =[]
         self.loss = []
-        self.summary_character = []
-        self.summary_loss = []
+        self.summary_handles = []
+        # self.summary_loss = []
         n_input = [self.input_size]+list(self.n_nodes[:-1])
         for i in range(self.n_layers):
+            summary_handle = SummaryHandle()
             layer = DAE(self.sess, n_input[i], noise=self.noise[i],units=self.n_nodes[i],
                         layer=i, n_epoch=self.n_epochs[i], is_training=self.is_training,
-                        input_dim=self.input_dim, batch_size=self.batch_size,learning_rate=self.lr[i])
+                        input_dim=self.input_dim, batch_size=self.batch_size,learning_rate=self.lr[i],
+                        summary_handle = summary_handle)
             self.loss.append(layer.loss)
             self.hidden_layers.append(layer)
             # self.summary_character.append(tf.summary.image(layer.next_x, "character" + str(i)))
-            self.summary_loss.append(tf.summary.scalar('loss',layer.loss))
+            # self.summary_loss.append(tf.summary.scalar('loss',layer.loss))
+            summary_handle.summ_loss.append(tf.summary.scalar('loss'+str(i),layer.loss))
+            self.summary_handles.append(summary_handle)
 
         # ------------------------ 提取各层可训练参数 -----------------------------------
         self.train_vals = tf.trainable_variables()
@@ -57,20 +76,37 @@ class SDAE(object):
         # ------------------------------------------------------------------------------
 
 
-    def train(self,x,valx):
+    def train(self,x,valx,shuffle=True):
         tf.global_variables_initializer().run()
         self.writer = tf.summary.FileWriter('./'+self.log_dir, self.sess.graph)
 
         input = x
+        if shuffle:
+            r = np.random.permutation(len(x))
+            input = x[r,:]
         # 保存原图
-        save_image(x, name=self.result_dir + '/original' + str(0) + '.png', n_show=self.n_show)
+        save_image(input, name=self.result_dir + '/original' + str(0) + '.png', n_show=self.n_show)
+        features = []
+        imgs = []
+
         for layer in self.hidden_layers:
             idx = self.hidden_layers.index(layer)
             print("training layer: ",idx )
-            layer.train(input,self.train_vals_layer[idx],
-                        summ_writer=self.writer,summ_loss=self.summary_loss[idx])
+            # layer.train(input,self.train_vals_layer[idx],
+            #             summ_writer=self.writer,summ_loss=self.summary_loss[idx])
+            layer.train(input, self.train_vals_layer[idx],
+                        summ_writer=self.writer, summ_handle=self.summary_handles[idx])
             input = layer.next_x
             # 保存重建图
 
             save_image(layer.rec,name = self.result_dir+'/rec'+str(idx)+'.png',n_show = self.n_show)
-            save_image(layer.next_x, name=self.result_dir + '/character' + str(idx) + '.png', n_show=self.n_show)
+            features.append(layer.next_x)
+            if idx == 0:
+                imgs.append(np.transpose(layer.features))
+            else:
+                imgs.append(np.dot(np.transpose(layer.features),imgs[idx-1]))
+            save_image(imgs[idx], name=self.result_dir + '/feature' + str(idx) + '.png', n_show=self.n_nodes[idx])
+        features = np.concatenate(tuple(features[1:]),axis = 1)
+        return features
+
+
